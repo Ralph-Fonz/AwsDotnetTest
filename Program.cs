e09b59b267a929ea11f8ec4185f58afc214e95fb
Ralph_Fonz <raphael@fonseca.tech>
2021-07-27 13:04:26 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AwsDotnetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
